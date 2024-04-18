371641b6d4dc02937508c478b5758542fc8f7f47
riorio805 <sefrianojieftara@gmail.com>
2024-04-19 01:52:46 +0700

    crosstown_bus 0.8.1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError, QueueProperties};
 use std::{thread, time};
+
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
     pub user_id: String,
     pub user_name: String
 }
+
 pub struct UserCreatedHandler;
 impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
-        let ten_millis = time::Duration::from_millis(1000);
-        let now = time::Instant::now();
-
+        // let ten_millis = time::Duration::from_millis(1000);
+        // let now = time::Instant::now();
         // thread::sleep(ten_millis);
 
-        println!("In Ade’s Computer [129500004y]. Message received: {:?}", message);
+        println!("In sefriano’s Computer [2206818966]. Message received: {:?}", message);
         Ok(())
     }
 }
+
 fn main() {
-    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
-    _ = listener.listen("user_created".to_owned(), UserCreatedHandler{}, crosstown_bus::QueueProperties {
-            auto_delete: false, durable: false, use_dead_letter: true });
+    let subscriber =
+        CrosstownBus::new_subscriber("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = subscriber.subscribe(
+            "user_created".to_owned(),
+            UserCreatedHandler{},
+            QueueProperties {
+                auto_delete: false,
+                durable: false,
+                use_dead_letter: true,
+                consume_queue_name: Some("queue2".to_string()),
+            });
 
     loop {
     }
